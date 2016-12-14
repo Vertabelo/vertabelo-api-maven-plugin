a798f82237f3d2cf7c32b07c880c06c024e858cc
Adam Mościcki <amoscicki@e-point.pl>
2016-12-14 13:11:17 +0100

    Copy from private repository

---
+++ README.md
@@ -0,0 +1,6 @@
+# vertabelo-api-maven-plugin
+A maven plugin to download xml and sql from Vertabelo
+
+## How to
+
+
---
+++ src/main/java/com/vertabelo/VertabeloSQLMojo.java
@@ -0,0 +1,85 @@
+package com.vertabelo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.Base64;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Mojo used to download SQL file from Vertabelo API based on following parameters:
+ * - destSQLFile - name of SQL destination file
+ * - apiToken: User's Vertabelo API Token
+ * - modelId: Id of the Vertabelo model
+ * - modelTag (optional): version tag of the model
+ *
+ * @author Krzysztof Waraksa
+ */
+@Mojo( name = "vertabeloSQL")
+public class VertabeloSQLMojo extends AbstractMojo
+{
+
+    public final String API_ADDR = "https://my.vertabelo.com/api/sql";
+
+    @Parameter
+    String apiToken;
+
+    @Parameter
+    String modelId;
+
+    @Parameter
+    String modelTag;
+
+    @Parameter(defaultValue = "model.sql")
+    String destSQLFile;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        if (apiToken == null) {
+            throw new MojoExecutionException("Vertabelo api token not set");
+        }
+        if (modelId == null) {
+            throw new MojoExecutionException("Vertabelo model id not set");
+        }
+
+        URL url;
+        try {
+            if (modelTag != null) {
+                url = new URL(API_ADDR + "/" + modelId + "/" + modelTag);
+            } else {
+                url = new URL(API_ADDR + "/" + modelId);
+            }
+        } catch (MalformedURLException ex) {
+            throw new MojoExecutionException("Incorrect URL parameters", ex);
+        }
+
+        try {
+            HttpURLConnection con = (HttpURLConnection) (url.openConnection());
+            String basicAuth = "Basic " + new String(new Base64().encode((apiToken + ":").getBytes()));
+            con.setRequestProperty("Authorization", basicAuth);
+            File file = new File("./" + destSQLFile);
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+            BufferedInputStream bis = new BufferedInputStream(con.getInputStream());
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            int i;
+            while ((i = bis.read()) != -1) {
+                bos.write(i);
+            }
+            bos.flush();
+            bis.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+    }
+}
